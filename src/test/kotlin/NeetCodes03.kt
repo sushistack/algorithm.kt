54e942e8733c67fa56158c72fb0df36761395543
shushistack <kjm71712@naver.com>
2023-11-30 18:40:27 +0900

    Add Problem: climbStairs

--- src/test/kotlin/NeetCodes03.kt
+++ src/test/kotlin/NeetCodes03.kt
@@ -1,9 +1,11 @@
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.lang.Exception
 import java.util.stream.Stream
 
 class NeetCodes03 {
@@ -37,12 +39,38 @@ class NeetCodes03 {
         numbers[index].used = false
     }
 
+    @Tag("Easy")
+    @Tag("1D DynamicProgramming")
+    @ParameterizedTest
+    @MethodSource("climbStairsProvider")
+    @DisplayName("""
+        You are climbing a staircase. It takes n steps to reach the top.
+        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    """)
+    fun climbStairs(n: Int, expected: Int) {
+        if (n == 1) return
+        val dp = Array(n + 1) { 0 }
+        dp[1] = 1
+        dp[2] = 2
+        for (i in 3..n) {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+        Assertions.assertThat(dp[n]).isEqualTo(expected)
+    }
+
     companion object {
         @JvmStatic
         fun subsetsProvider(): Stream<Arguments> =
             Stream.of(
                 arguments(intArrayOf(1,2,3), listOf(listOf(), listOf(1), listOf(2), listOf(1, 2), listOf(3), listOf(1, 3), listOf(2, 3), listOf(1, 2, 3)))
         )
+
+        @JvmStatic
+        fun climbStairsProvider(): Stream<Arguments> =
+            Stream.of(
+                arguments(2, 2),
+                arguments(3, 3)
+            )
     }
 
     class NumberStatus(
