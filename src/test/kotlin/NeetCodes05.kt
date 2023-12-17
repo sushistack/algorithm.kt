8bf4290ab3fecc9dd449114ca216359c52886d52
shushistack <kjm71712@naver.com>
2023-12-17 20:34:45 +0900

    Add Problem: lastStoneWeight

--- src/test/kotlin/NeetCodes05.kt
+++ src/test/kotlin/NeetCodes05.kt
@@ -161,6 +161,40 @@ class NeetCodes05 {
 
     data class Point(val r: Int, val c: Int)
 
+    @Tag("Easy")
+    @Tag("Heap")
+    @ParameterizedTest
+    @MethodSource("lastStoneWeightProvider")
+    @DisplayName("""You are given an array of integers stones where stones[i] is the weight of the ith stone.
+        We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together.
+        Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+        If x == y, both stones are destroyed, and
+        If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+        At the end of the game, there is at most one stone left. 
+        Return the weight of the last remaining stone. If there are no stones left, return 0.
+    """)
+    fun lastStoneWeight(stones: IntArray, expected: Int) {
+        val pq = PriorityQueue<Int> { o1, o2 -> o2 - o1 }
+        for (i in stones.indices) {
+            pq.add(stones[i])
+        }
+
+        var answer = 0
+        var cur: Int
+        var next: Int
+        while (pq.isNotEmpty()) {
+            cur = pq.poll()
+            if (pq.isEmpty()) {
+                answer = cur
+                break
+            }
+            next = pq.poll()
+            pq.add(cur - next)
+        }
+
+        Assertions.assertThat(answer).isEqualTo(expected)
+    }
+
     companion object {
         var maxDepth = 0
         var combinations = mutableListOf<List<Int>>()
@@ -235,6 +269,13 @@ class NeetCodes05 {
                     ),
                     4
                 )
+            )
+
+        @JvmStatic
+        fun lastStoneWeightProvider(): Stream<Arguments> =
+            Stream.of(
+                arguments(intArrayOf(2,7,4,1,8,1), 1),
+                arguments(intArrayOf(1), 1)
             )
     }
 
