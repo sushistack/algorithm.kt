8f3952f7ffab6784551c51b398737ac290371cfb
shushistack <kjm71712@naver.com>
2023-12-21 20:32:11 +0900

    Add Problem: longestCommonSubsequence

--- src/test/kotlin/NeetCodes03.kt
+++ src/test/kotlin/NeetCodes03.kt
@@ -154,7 +154,7 @@ class NeetCodes03 {
 
 
     @Tag("Medium")
-    @Tag("2D DynamicProgramming")
+    @Tag("2D_DynamicProgramming")
     @ParameterizedTest
     @MethodSource("uniquePathsProvider")
     @DisplayName("""There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
--- src/test/kotlin/NeetCodes06.kt
+++ src/test/kotlin/NeetCodes06.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
+import kotlin.math.max
 
 class NeetCodes06 {
 
@@ -31,6 +32,32 @@ class NeetCodes06 {
         Assertions.assertThat(cur == 0).isEqualTo(expected)
     }
 
+    @Tag("Medium")
+    @Tag("2D_DynamicProgramming")
+    @ParameterizedTest
+    @MethodSource("longestCommonSubsequenceProvider")
+    @DisplayName("""Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+        A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+        For example, "ace" is a subsequence of "abcde".
+        A common subsequence of two strings is a subsequence that is common to both strings.
+    """)
+    fun longestCommonSubsequence(text1: String, text2: String, expected: Int) {
+        val dp = Array(text1.length + 1) { IntArray(text2.length + 1) { 0 } }
+
+        for (i in 1..text1.length) {
+            for (j in 1..text2.length) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                } else {
+                    dp[i][j] = maxOf(dp[i - 1][j], dp[i][j - 1])
+                }
+            }
+        }
+
+        Assertions.assertThat(dp[text1.length][text2.length]).isEqualTo(expected)
+    }
+
+
     companion object {
         @JvmStatic
         fun canJumpProvider(): Stream<Arguments> =
@@ -42,6 +69,17 @@ class NeetCodes06 {
                 arguments(intArrayOf(3,0,8,2,0,0,1), true),
                 arguments(intArrayOf(4,2,0,0,1,1,4,4,4,0,4,0), true),
                 arguments(intArrayOf(1,2,3), true)
+            )
+
+        @JvmStatic
+        fun longestCommonSubsequenceProvider(): Stream<Arguments> =
+            Stream.of(
+                arguments("abcde", "ace", 3),
+                arguments("abc", "abc", 3),
+                arguments("abc", "def", 0),
+                arguments("bl", "yby", 1),
+                arguments("a", "n", 0),
+                arguments("ezupkr", "ubmrapg", 2)
             )
     }
 
