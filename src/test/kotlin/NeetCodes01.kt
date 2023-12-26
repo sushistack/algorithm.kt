3b006d2b5652905f18b1e2114b96cec48865e629
sushistack <kjm71712@naver.com>
2023-12-26 20:41:10 +0900

    change indent

--- src/test/kotlin/NeetCodes01.kt
+++ src/test/kotlin/NeetCodes01.kt
@@ -117,10 +117,11 @@ class NeetCodes01 {
     @ParameterizedTest
     @MethodSource("maxProfitProvider")
     @DisplayName("""
-    You are given an array prices where prices[i] is the price of a given stock on the ith day.
-    You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-""")
+        You are given an array prices where prices[i] is the price of a given stock on the ith day.
+        You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+        Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+    """
+    )
     fun maxProfit(prices: IntArray, expected: Int) {
         var maxProfit = 0
         for (i in prices.indices) {
